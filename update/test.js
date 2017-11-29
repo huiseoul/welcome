acdfc5505e4a2254a8513a660eb8f9c8e16dd38e
Johnwook Choi <johnwook.choi@gmail.com>
2017-11-29 14:59:03 +0900

    anonymous function 을 () => 으로 변경

--- update/test.js
+++ update/test.js
@@ -1,12 +1,12 @@
 var assert = require("assert");
 var update = require("./");
 
-describe("update", function() {
-  describe("has a #$set method that", function() {
+describe("update", () => {
+  describe("has a #$set method that", () => {
     var state;
     var commands;
     var nextState;
-    beforeEach(function() {
+    beforeEach(() => {
       state = {
         a: {
           b: 22,
@@ -18,44 +18,44 @@ describe("update", function() {
       nextState = update(state, commands);
     });
 
-    it("changes the tree on the directive", function() {
+    it("changes the tree on the directive", () => {
       assert(state.a.c !== nextState.a.c);
     });
 
-    it("reuses state on different branches", function() {
+    it("reuses state on different branches", () => {
       assert(state.unChanged === nextState.unChanged);
     });
 
-    it("reuses state on same level", function() {
+    it("reuses state on same level", () => {
       assert(state.a.b === state.a.b);
     });
   });
 
-  describe("can pass react's test suite", function() {
-    it("should support push", function() {
+  describe("can pass react's test suite", () => {
+    it("should support push", () => {
       assert.deepEqual(update([1], { $push: [7] }), [1, 7]);
     });
 
-    it("should support unshift", function() {
+    it("should support unshift", () => {
       assert.deepEqual(update([1], { $unshift: [7] }), [7, 1]);
     });
 
-    it("should support splice", function() {
+    it("should support splice", () => {
       assert.deepEqual(update([1, 4, 3], { $splice: [[1, 1, 2]] }), [1, 2, 3]);
     });
 
-    it("should support merge", function() {
+    it("should support merge", () => {
       assert.deepEqual(update({ a: "b" }, { $merge: { c: "d" } }), {
         a: "b",
         c: "d"
       });
     });
 
-    it("should support set", function() {
+    it("should support set", () => {
       assert.deepEqual(update({ a: "b" }, { $set: { c: "d" } }), { c: "d" });
     });
 
-    it("should support apply", function() {
+    it("should support apply", () => {
       assert.equal(
         update(2, {
           $apply: function(x) {
@@ -66,7 +66,7 @@ describe("update", function() {
       );
     });
 
-    it("should support deep updates", function() {
+    it("should support deep updates", () => {
       assert.deepEqual(
         update({ a: "b", c: { d: "e" } }, { c: { d: { $set: "f" } } }),
         {
@@ -76,7 +76,7 @@ describe("update", function() {
       );
     });
 
-    it("should perform safe hasOwnProperty check", function() {
+    it("should perform safe hasOwnProperty check", () => {
       assert.deepEqual(update({}, { hasOwnProperty: { $set: "a" } }), {
         hasOwnProperty: "a"
       });
